13eff11febc71c4101c0f54d7e3df96167c575dc
Ryuyxx <Ryuyxx@gmail.com>
2021-07-21 12:55:11 +0900

    :sparkles: GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,26 +21,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				{GA_TRACKING_ID && (
-					<>
-						<script
-							async
-							src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-						/>
-						<script
-							dangerouslySetInnerHTML={{
-								__html: `
-										window.dataLayer = window.dataLayer || [];
-										function gtag(){dataLayer.push(arguments);}
-										gtag('js', new Date());
-										gtag('config', '${GA_TRACKING_ID}', {
-										page_path: window.location.pathname,
-										});
-									`,
-							}}
-						/>
-					</>
-				)}
 				<link rel="favicon" href="/favicon.ico" />
 			</Head>
 			<Component {...pageProps} />
